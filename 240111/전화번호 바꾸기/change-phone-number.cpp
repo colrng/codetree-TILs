dfe442156ed5153791984328cf2c49f991af3cf2
colrng <149384882+colrng@users.noreply.github.com>
2024-01-11 16:44:17 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a,x,y;
+
+    cin>>a;
+    cin.get();
+    cin>>x;
+    cin.get();
+    cin>>y;
+
+    cout<<0<<a<<"-"<<y<<"-"<<x;
+
+    return 0;
+}
