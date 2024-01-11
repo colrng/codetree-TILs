3f80841a639427d783469d1a3532af548323cd5c
colrng <149384882+colrng@users.noreply.github.com>
2024-01-11 16:35:06 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int m,d,y;
+
+    cin>>y;
+    cin.get();
+    cin>>m;
+    cin.get();
+    cin>>d;
+    cin.get();
+
+    cout<<m<<"-"<<d<<"-"<<y;
+
+    return 0;
+}
