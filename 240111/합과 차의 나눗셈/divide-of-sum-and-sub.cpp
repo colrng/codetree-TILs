806c6571821be0794c268da36c1fb3815fcf5186
colrng <149384882+colrng@users.noreply.github.com>
2024-01-11 16:54:47 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
+++ 240111/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -4,15 +4,13 @@ using namespace std;
 
 int main() {
 
-    double a, b;
+    int a, b;
     cin >> a >> b;
-    double c = a + b;
-    double z = a - b;
 
     cin >> fixed;
     cout.precision(3);
 
-    cout << c / z;
+    cout << (double) (a+b)/(a-b);
 
     return 0;
 }
