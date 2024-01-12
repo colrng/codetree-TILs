6e953044c2764e8d95a33cb59505ba2480c6b1aa
colrng <149384882+colrng@users.noreply.github.com>
2024-01-12 19:47:49 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a;
+    cin>>a;
+
+    if (a<100)
+        cout<<"failure";
+
+    else cout << "pass";
+
+    return 0;
+}
