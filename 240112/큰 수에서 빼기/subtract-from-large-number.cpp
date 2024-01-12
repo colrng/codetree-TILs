f09612708087105d80df2fae5a7e04edd62318d8
colrng <149384882+colrng@users.noreply.github.com>
2024-01-12 17:25:15 +0900

    Title: 큰 수에서 빼기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240112/큰 수에서 빼기/subtract-from-large-number.cpp
+++ 240112/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main() {
     
-    int a,b;
+    int a,b,c;
 
     cin>>a>>b;
-
-    cout <<-(a-b);
+    
+    c=a-b;
+    
+    if (c<0) {
+        cout<<-c;
+    }
 
     return 0;
 }
