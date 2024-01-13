e4dbd1fa75894f20b6409f67de478e09ddde5077
colrng <149384882+colrng@users.noreply.github.com>
2024-01-13 20:47:04 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240113/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+    int a;
+    cin >> a;
+    
+    if(a == 5){
+        cout<<"A";
+    }
+    if(a % 2 == 0){
+        cout<<"B";
+    }
+    cout << "";
+    return 0;
+}
