8240f406394127788a77c77bd7fcc47840f65c3c
colrng <149384882+colrng@users.noreply.github.com>
2024-01-17 18:12:42 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240117/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b;
+	cin >> a,b;
+
+	if (a == 0) {
+		if (b <= 19) {
+			cout << "MAN" << endl;
+		}
+		else {
+			cout << "BOY" << endl;
+		}
+	}
+	else {
+		if (b >= 19) {
+			cout << "WOMAN" << endl;
+		}
+		else {
+			cout << "GIRL" << endl;
+		}
+	}
+
+	return 0;
+
+}
