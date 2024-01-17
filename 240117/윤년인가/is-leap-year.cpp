1fc5cc85e7da660895150e008f05a89abbfeaa01
colrng <149384882+colrng@users.noreply.github.com>
2024-01-17 18:24:29 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240117/윤년인가/is-leap-year.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a;
+    cin >> a;
+
+    if (a % 4 == 0){
+
+    }
+    
+    if (a % 100 == 0){
+        cout << "false" << endl;
+    }
+    else cout << "true" << endl;
+
+
+    return 0;
+}
