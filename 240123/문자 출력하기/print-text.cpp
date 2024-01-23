50be968ab902e7377d70ec0d604bcdea928bdc22
colrng <149384882+colrng@users.noreply.github.com>
2024-01-23 16:43:23 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/문자 출력하기/print-text.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    char a;
+
+	// 입력
+	cin >> a;
+    
+    // 출력
+	for(int i = 1; i <= 8; i++)
+		cout << a;
+
+    return 0;
+}
