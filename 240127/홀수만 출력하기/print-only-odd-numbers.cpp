e358a6964de8847a601835df46396366595a7b12
colrng <149384882+colrng@users.noreply.github.com>
2024-01-27 20:31:35 +0900

    Title: 홀수만 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240127/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int num;
+        cin >> num;
+
+        if (num % 2 == 1 && num % 3 == 0) {
+            cout << num << endl;
+        }
+    }
+
+    cout << std::endl;
+
+    return 0;
+}
