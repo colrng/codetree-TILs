3ed504f10d3bd75126286a43dd61d72b8dcb0451
colrng <149384882+colrng@users.noreply.github.com>
2024-01-28 09:05:10 +0900

    Title: 배수의 개수 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240128/배수의 개수 2/number-of-multipliers-2.cpp
+++ 240128/배수의 개수 2/number-of-multipliers-2.cpp
@@ -6,8 +6,9 @@ int main() {
 	int n;
 	int cnt = 0;
 
-	for (int i = 1; i < 11; i++) {
-		if (i % 2 == 1) {
+	for (int i = 1; i < 10; i++) {
+		cin >> n;
+		if (i % 2 == 1 || i / 10 == 1) {
 			cnt++;
 		}
 	}
